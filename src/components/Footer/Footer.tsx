a9fa8f34c82eb7e01c94aa710d0a14531a0fafc6
Leonardo Perez <leperez@google.com>
2023-02-22 14:13:58 -0500

    fix no referrer bug in a tags

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,19 +11,28 @@ function Footer({ hideLocation, hidePhone }: { hideLocation?: boolean; hidePhone
       <div className={styles.separator} />
       <p className={styles.firstLine}>Síguenos en / Follow us on:</p>
       <div className={styles.socialNetworksBox}>
-        <a href='https://www.instagram.com/lascalidosasburger/' target='_blank'>
+        <a
+          href='https://www.instagram.com/lascalidosasburger/'
+          target='_blank'
+          rel='noreferrer'
+        >
           <SocialNetwork icon={FiInstagram} text='@lascalidosasburger' />
         </a>
         <a
           href='https://www.facebook.com/profile.php?id=100081292250093'
           target='_blank'
+          rel='noreferrer'
         >
           <SocialNetwork icon={ImFacebook} text='lascalidosasburger' />
         </a>
       </div>
       {!hideLocation ? (
         <p>
-          <a href='https://goo.gl/maps/SsPjhVtvM9vRK9Kd7' target='_blank'>
+          <a
+            href='https://goo.gl/maps/SsPjhVtvM9vRK9Kd7'
+            target='_blank'
+            rel='noreferrer'
+          >
             <MdOutlineLocationOn />
             13 Center St. Elizabeth New Jersey
           </a>
@@ -31,7 +40,7 @@ function Footer({ hideLocation, hidePhone }: { hideLocation?: boolean; hidePhone
       ) : null}
       {!hidePhone ? (
         <p>
-          <a href='https://wa.me/19735101954' target='_blank'>
+          <a href='https://wa.me/19735101954' rel='noreferrer' target='_blank'>
             <FaWhatsapp /> (973) 510 1954
           </a>
         </p>

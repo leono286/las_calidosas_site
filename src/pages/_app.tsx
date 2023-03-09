53b2fb82e0504b31b8941de1b6d8f723039e228a
Leonardo Perez <leperez@google.com>
2023-03-09 12:26:32 -0500

    Add metatags to Head aiming to improve SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import localFont from '@next/font/local'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import LogoGreeting from '@/components/LogoGreeting'
-
+import Head from 'next/head'
 
 const gothamUltra = localFont({
   src: './../assets/fonts/gotham-ultra-webfont.woff2',
@@ -20,11 +20,11 @@ const dorchesterDisplay = localFont({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   const router = useRouter()
-  const printMode = router.query.hasOwnProperty('printMode')
-  const [showLogo, setShowLogo] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const routerQuery = router.query
+  const printMode = routerQuery.hasOwnProperty('printMode')
+  const [showLogo, setShowLogo] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const alreadyShown = window.sessionStorage.getItem('logoShown')
@@ -33,32 +33,56 @@ export default function App({ Component, pageProps }: AppProps) {
       window.sessionStorage.setItem('logoShown', 'true')
     }
     setIsLoading(false)
-  }, []);
+  }, [])
 
   useEffect(() => {
     if (showLogo && !isLoading) {
       setTimeout(() => {
         setShowLogo(false)
-      }, 1200);
+      }, 1200)
     }
-  }, [isLoading, showLogo]);
-  
+  }, [isLoading, showLogo])
+
   return (
-    <main
-      className={`${gothamBold.variable} ${gothamUltra.variable} ${dorchesterDisplay.variable}`}
-    >
-      <style>
-        {printMode
-          ? `
+    <>
+      <Head>
+        <title>Las Calidosas Burger</title>
+        <meta name='description' content='Las calidosas burger website' />
+        <meta
+          name='keywords'
+          content='colombian fast food, colombian food, fast food, comida rápida colombiana, comida colombiana, comida rápida,comida colombiana Elizabeth NJ'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:url' content='http://lascalidosas.com' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Las Calidosas Burger' />
+        <meta
+          property='og:description'
+          content='Sabor a calle colombiana. Comida rápida colombiana en Elizabeth NJ. Sabor a calle colombiana.'
+        />
+        <meta property='og:image' content='/hamburger.jpg' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main
+        className={`${gothamBold.variable} ${gothamUltra.variable} ${dorchesterDisplay.variable}`}
+      >
+        <style>
+          {printMode
+            ? `
         :root {
           --brand-yellow: #111011;
           --white: #111011;
           --brand-background: #fff;
         }
         `
-          : ''}
-      </style>
-      {isLoading ? null : showLogo ?  <LogoGreeting/>: <Component {...pageProps} />}
-    </main>
+            : ''}
+        </style>
+        {isLoading ? null : showLogo ? (
+          <LogoGreeting />
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </main>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Las Calidosas Burger </title>
+        <title>Las Calidosas Burger</title>
         <meta name='description' content='Las calidosas burger website' />
         <meta name="keywords" content="colombian fast food, colombian food, fast food, comida rápida colombiana, comida colombiana, comida rápida" />
         <meta name='viewport' content='width=device-width, initial-scale=1' />

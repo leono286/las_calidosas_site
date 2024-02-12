54aac909ec6e3ddb04ae380872d8ab3b24a91379
Leonardo Perez <leperez@google.com>
2024-02-12 17:54:28 -0500

    PWA first attemp no fonts caching for offline.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -23,4 +24,12 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWA(nextConfig)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,15 @@ import Head from 'next/head'
 
 const gothamUltra = localFont({
   src: './../assets/fonts/gotham-ultra-webfont.woff2',
-  variable: '--gotham-ultra-font',
+  variable: '--gotham-ultra-font'
 })
 const gothamBold = localFont({
   src: './../assets/fonts/gotham-bold-webfont.woff2',
-  variable: '--gotham-bold-font',
+  variable: '--gotham-bold-font'
 })
 const dorchesterDisplay = localFont({
   src: './../assets/fonts/dorchesterdisplay-webfont.woff2',
-  variable: '--dorchester-display-font',
+  variable: '--dorchester-display-font'
 })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -47,16 +47,34 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Las Calidosas Burger</title>
-        <meta name='description' content='Las calidosas burger website' />
+        <meta
+          name='description'
+          content='Las calidosas burger website'
+        />
         <meta
           name='keywords'
           content='colombian fast food, colombian food, fast food, comida rápida colombiana, comida colombiana, comida rápida,comida colombiana Elizabeth NJ'
         />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta property='og:url' content='https://lascalidosas.com' />
-        <meta property='og:type' content='website' />
-        <meta property='og:site_name' content='Las Calidosas Burger'></meta>
-        <meta property='og:title' content='Las Calidosas Burger' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
+        <meta
+          property='og:url'
+          content='https://lascalidosas.com'
+        />
+        <meta
+          property='og:type'
+          content='website'
+        />
+        <meta
+          property='og:site_name'
+          content='Las Calidosas Burger'
+        ></meta>
+        <meta
+          property='og:title'
+          content='Las Calidosas Burger'
+        />
         <meta
           property='og:description'
           content='Sabor a calle colombiana. Comida rápida colombiana en Elizabeth NJ.'
@@ -69,12 +87,38 @@ export default function App({ Component, pageProps }: AppProps) {
           property='og:image:secure_url'
           content='https://lascalidosas.com/hamburger.jpg'
         />
-        <meta property='og:image:type' content='image/jpeg' />
+        <meta
+          property='og:image:type'
+          content='image/jpeg'
+        />
         <meta
           property='og:image:alt'
           content='Hamburger image with text "Sabor a calle colombiana"'
         />
-        <link rel='icon' href='/favicon.ico' />
+
+        <link
+          rel='icon'
+          href='/images/favicon.ico'
+          sizes='48x48'
+        />
+        <link
+          rel='icon'
+          href='/images/burger-logo.svg'
+          sizes='any'
+          type='image/svg+xml'
+        />
+        <link
+          rel='apple-touch-icon'
+          href='/images/apple-touch-icon-180x180.png'
+        />
+        <link
+          rel='manifest'
+          href='/manifest.json'
+        />
+        <meta
+          name='theme-color'
+          content='#111011'
+        />
       </Head>
       <main
         className={`${gothamBold.variable} ${gothamUltra.variable} ${dorchesterDisplay.variable}`}

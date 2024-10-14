40ace34687200d61760738f97b6fac4a74a9722e
Leonardo Perez <leperez@google.com>
2024-10-14 12:41:02 -0400

    Add second place phone number

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import styles from './Footer.module.scss';
 import { ImFacebook } from 'react-icons/im';
 import { FiInstagram } from 'react-icons/fi';
-import { MdOutlineLocationOn, MdOutlineShoppingBag, MdRestaurant } from 'react-icons/md';
+import {
+  MdOutlineLocationOn,
+  MdOutlineShoppingBag,
+  MdRestaurant,
+} from 'react-icons/md';
 import { FaWhatsapp } from 'react-icons/fa';
 import { TypeFooter } from '@/Types';
 import DeliveryPlatformLink from '../DeliveryPlatformLink';
@@ -26,6 +30,8 @@ function Footer(props?: TypeFooter) {
           url='https://www.facebook.com/profile.php?id=100081292250093'
         />
       </div>
+
+      <div className={styles.separator} />
       <p>
         <a
           href='https://maps.app.goo.gl/jzqeCFv89ZwihHRd9'
@@ -33,10 +39,20 @@ function Footer(props?: TypeFooter) {
           rel='noreferrer'
         >
           <MdOutlineLocationOn />
-          37 S Second St. Elizabeth New Jersey
-          &nbsp;( <MdRestaurant />&nbsp;-&nbsp;<MdOutlineShoppingBag />)
+          37 S Second St. Elizabeth New Jersey &nbsp;( <MdRestaurant />
+          &nbsp;-&nbsp;
+          <MdOutlineShoppingBag />)
         </a>
       </p>
+
+      <p>
+        <a href='https://wa.me/18622208239' rel='noreferrer' target='_blank'>
+          <FaWhatsapp /> (862) 220 8239
+        </a>
+      </p>
+
+      <div className={styles.separator} />
+      
       <p>
         <a
           href='https://goo.gl/maps/SsPjhVtvM9vRK9Kd7'
@@ -44,17 +60,12 @@ function Footer(props?: TypeFooter) {
           rel='noreferrer'
         >
           <MdOutlineLocationOn />
-          13 Center St. Elizabeth New Jersey
-          &nbsp;( <MdOutlineShoppingBag />)
+          13 Center St. Elizabeth New Jersey &nbsp;( <MdOutlineShoppingBag />)
         </a>
       </p>
 
       <p>
-        <a
-          href='https://wa.me/19735101954'
-          rel='noreferrer'
-          target='_blank'
-        >
+        <a href='https://wa.me/19735101954' rel='noreferrer' target='_blank'>
           <FaWhatsapp /> (973) 510 1954
         </a>
       </p>
@@ -67,10 +78,7 @@ function Footer(props?: TypeFooter) {
           </p>
           <div className={styles.deliveryPartnersBox}>
             {deliveryPlatformsList.map((platform) => (
-              <DeliveryPlatformLink
-                key={platform.sys.id}
-                {...platform}
-              />
+              <DeliveryPlatformLink key={platform.sys.id} {...platform} />
             ))}
           </div>
         </>
--- src/components/NewLocationAnnouncement/NewLocationAnnouncement.tsx
+++ src/components/NewLocationAnnouncement/NewLocationAnnouncement.tsx
@@ -1,6 +1,7 @@
 import styles from './NewLocationAnnouncement.module.scss';
 import CelebrateIcon from '@/assets/svg/celebrationIcon.svg';
 import { MdOutlineLocationOn } from 'react-icons/md';
+import { FaWhatsapp } from 'react-icons/fa';
 
 function NewLocationAnnouncement() {
   return (
@@ -9,10 +10,12 @@ function NewLocationAnnouncement() {
         <CelebrateIcon />
       </div>
       <div className={styles.content}>
-        <h2>Caé y parchamos <br /> en nuestro nuevo local!!</h2>
+        <h2>
+          Caé y parchamos <br /> en nuestro nuevo local!!
+        </h2>
         <p className={styles.text}>
           Abrimos un segundo local donde puedes venir y disfrutar en familia de
-          nuestro tradicional sabor a calle colombiana. <br/> Te esperamos!.
+          nuestro tradicional sabor a calle colombiana. <br /> Te esperamos!.
         </p>
         <p className={styles.location}>
           <a
@@ -20,8 +23,13 @@ function NewLocationAnnouncement() {
             target='_blank'
             rel='noreferrer'
           >
-            <MdOutlineLocationOn size={28}/>
+            <MdOutlineLocationOn size={28} />
             37 S Second St. Elizabeth New Jersey
+          </a>
+        </p>
+        <p className={styles.phone}>
+          <a href='https://wa.me/18622208239' rel='noreferrer' target='_blank'>
+            <FaWhatsapp /> (862) 220 8239
           </a>
         </p>
       </div>
